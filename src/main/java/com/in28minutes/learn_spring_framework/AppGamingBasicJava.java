4a3609a9a0b529d07339a39649939eb448f6fee8
Aravind Anand <itsaravindanand@gmail.com>
2024-06-13 13:40:14 -0700

    Loose Coupling Using Interface Code Demonstration

--- src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -1,18 +1,23 @@
 package com.in28minutes.learn_spring_framework;
 
-import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.MarioGame;
-import com.in28minutes.learn_spring_framework.game.SuperContraGame;
+import com.in28minutes.learn_spring_framework.game.*;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        //Tight Coupling demonstration
-        //var marioGame = new MarioGame();
+//        Tight Coupling demonstration
+        var marioGame = new MarioGame();
         var superContraGame = new SuperContraGame();
+        var pacmanGame = new PacmanGame();
 
-        //var gameRunner = new GameRunner(marioGame);
-        var gameRunner = new GameRunner(superContraGame);
+//        Using marioGame or superContraGame needs changes in the GameRunner Class as there is no common interface
+//        var gameRunner = new GameRunner(marioGame);
+//        var gameRunner = new GameRunner(superContraGame);
+//        var gameRunner = new GameRunner(pacmanGame);
 
+//        this is after the implementation of interface
+//        No need to change the Objects in GameRunner class as we used interface there
+//        Just change the object passed into the GameRunner Constructor to execute the respective implementation
+        var gameRunner = new GameRunner(pacmanGame);
         gameRunner.run();
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
@@ -1,19 +1,19 @@
 package com.in28minutes.learn_spring_framework.game;
 
-public class SuperContraGame implements GamingConsole {
+public class PacmanGame implements GamingConsole{
     public void up() {
         System.out.println("Go Up");
     }
 
     public void down() {
-        System.out.println("Sit Down");
+        System.out.println("Go Down");
     }
 
     public void left() {
-        System.out.println("Go Back");
+        System.out.println("Go Left");
     }
 
     public void right() {
-        System.out.println("Shoot Bullet");
+        System.out.println("Go Right");
     }
 }
